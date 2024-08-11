689b85b393ceebebe6680d108df15dc5de4536ef
jonathanduran7 <joniduran624@gmail.com>
2024-08-10 21:39:17 -0300

    login controller

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,5 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import { createUserValidator } from '#validators/user'
+import { createUserValidator, loginValidator } from '#validators/user'
 import User from '#models/user.model'
 
 export default class UsersController {
@@ -7,5 +7,24 @@ export default class UsersController {
     const payload = await request.validateUsing(createUserValidator)
     await User.create(payload)
     response.ok({ message: 'User created successfully' })
+  }
+
+  async login({ request, response }: HttpContext) {
+    const payload = await request.validateUsing(loginValidator)
+
+    const user = await User.query().where('email', payload.email).first()
+    if (!user) {
+      response.badRequest({ message: 'Password or email wrong' })
+      return
+    }
+
+    const passwordVerified = await User.verifyPassword(payload.password, user?.password)
+
+    if (!passwordVerified) {
+      response.badRequest({ message: 'Password or email wrong' })
+      return
+    }
+
+    response.ok({ message: 'Logged in successfully' })
   }
 }
